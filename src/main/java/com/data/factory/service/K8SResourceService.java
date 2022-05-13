2b1956eb8b00f91f0cef4546062605cfdc8d2b3e
lmbarrera <lmbarrera@falabella.cl>
2022-05-13 17:16:39 -0400

    Deleted unused import.

--- src/main/java/com/data/factory/service/K8SResourceService.java
+++ src/main/java/com/data/factory/service/K8SResourceService.java
@@ -2,7 +2,6 @@ package com.data.factory.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
